be46f34e2d3dfd7d3b9565892d06d6b63e1af5c7
Fatara1219 <67969041+Fatara1219@users.noreply.github.com>
2020-08-06 10:53:37 -0500

    Add files via upload

---
+++ MyFatara_M_TabbedApp_Exercise/MyFatara_M_TabbedApp_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFatara_M_TabbedApp_Exercise
+//
+//  Created by Fatara Muhammad on 8/6/20.
+//  Copyright © 2020 Fatara Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
